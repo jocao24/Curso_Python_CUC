153c9c7f5a063f912ba43cc7d8bd972cd7298be9
Jocao <jcaicedo50@gmail.com>
2019-04-27 11:44:38 -0500

    condicionales

--- holaMundo.py
+++ holaMundo.py
@@ -1,37 +1,18 @@
 
-# variables
-x = 10
-y = "Texto"
-z = 4.5
-a = True
-b = False
-c = 4+5j
+a = 5
+b = 5
 
-"""
-print(x)
-print(y)
-print(z)
-print(a)
-print(b)
-print(c)
+if a == b:
+    print (a," es igual a b ",b)
+elif a > b: 
+    print (a," es mayor que ",b)
+else:
+    print(b, " es menor que ",a)
+    
+if a == b and a>2:
+    print(a," es igual a ",b, " y es mayor a 2")
 
-"""
-print (x, y, z, a, b,c)
-print (type(b))
 
-# conversión de variables 
 
-num1 = int(2)
-print(num1)
-
-num2 = int(2.8)
-print(num2)
-
-var1 = int("5")
-var2 = int("8")
-print (var1 + var2)
-
-var3 = float("2.5")
-print (var3 + z)
 
 
