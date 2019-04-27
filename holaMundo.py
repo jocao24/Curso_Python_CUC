8796c125b40a5bde9cea239510c20419ab4172c3
Jocao <jcaicedo50@gmail.com>
2019-04-27 10:44:07 -0500

    Foto inicial 2

--- holaMundo.py
+++ holaMundo.py
@@ -1,2 +1,37 @@
-a = 5
-A = 7
+
+# variables
+x = 10
+y = "Texto"
+z = 4.5
+a = True
+b = False
+c = 4+5j
+
+"""
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
+print(c)
+
+"""
+print (x, y, z, a, b,c)
+print (type(b))
+
+# conversión de variables 
+
+num1 = int(2)
+print(num1)
+
+num2 = int(2.8)
+print(num2)
+
+var1 = int("5")
+var2 = int("8")
+print (var1 + var2)
+
+var3 = float("2.5")
+print (var3 + z)
+
+
